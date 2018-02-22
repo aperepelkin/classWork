957221d6e575852bedc0b73a0a02780174f3e6d1
sergei <sergei@gmail.com>
2018-02-22 21:38:19 +0300

    lesson 30, treeset

--- src/lesson30/Work02.java
+++ src/lesson30/Work02.java
@@ -2,16 +2,29 @@ package lesson30;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Work02 {
+	
+	static class PersonComparator implements Comparator<Person> {
+		@Override
+		public int compare(Person o1, Person o2) {
+			return o2.name.compareTo(o1.name);
+		}
+	}
 
 	public static void main(String[] args) {
-
-		Set<Person> set = new TreeSet<>();
+		
+		Set<Person> set = new TreeSet<>(new Comparator<Person>() {
+			@Override
+			public int compare(Person o1, Person o2) {
+				return o2.name.compareTo(o1.name);
+			}
+		});
 		set.add(new Person("n3"));
 		set.add(new Person("n3"));
 		set.add(new Person("n1"));
