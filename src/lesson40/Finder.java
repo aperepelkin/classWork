4dd0fe75b037e559abd7b51cde59ea6ced559ca4
sergei <sergei@gmail.com>
2018-03-29 21:59:35 +0300

    Template for thread

--- src/lesson40/Finder.java
+++ src/lesson40/Finder.java
@@ -13,14 +13,38 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Finder {
+	class SearchWorker implements Runnable {
+
+		List<Integer> positions = new ArrayList<>();
+		
+		private Integer number;
+		private String line;
+		private String word;
+		
+		public SearchWorker(Integer number, String line, String word) {
+			this.number = number;
+			this.line = line;
+			this.word = word;
+		}
+
+		@Override
+		public void run() {
+			String[] words = line.split(" ");
+			for(int i = 0; i < words.length; i++) {
+				if(words[i].equals(word))
+					positions.add(i);
+			}
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
+		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Путь к файлу");
 		String fileName = sc.nextLine();
 		System.out.println("Слово");
 		String word = sc.nextLine();
-		
+		long startTime = System.nanoTime();
 		FileInputStream fin = new FileInputStream(fileName);
 		LineNumberReader reader = new LineNumberReader(
 				new InputStreamReader(fin));
@@ -34,6 +58,7 @@ public class Finder {
 				enters.put(number, finds);
 		}
 		
+		System.out.println("Duration: " + (System.nanoTime() - startTime));
 	}
 	
 	private static List<Integer> find(String line, String word) {
